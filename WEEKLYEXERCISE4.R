f84167251921be59e5e2a11136c6960aac8e985f
ricardojoeson <1287883296@qq.com>
2025-09-24 14:37:20 -0500

    7.a.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,3 +1,18 @@
 library(usethis)
-
+library(readr)
+library(dplyr)
+library(ggplot2)
 # main
+
+olydata = read.csv("D:/MSCA/PA446/week3/Olympics.csv")
+
+# 7.a.
+largest_1992 = olydata %>%
+  filter(year == 1992) %>%
+  arrange(desc(athletes))
+largest_1992[1,"country"]
+
+# a_c stands for athletes and country
+a_c = tibble(
+  largest_1992[,c("athletes","country")]
+)
