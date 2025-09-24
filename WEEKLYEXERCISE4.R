9d54397333034ca2a51d248d5c96d55d75a31716
ricardojoeson <1287883296@qq.com>
2025-09-24 14:23:43 -0500

    6.a.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,3 +1,11 @@
 library(usethis)
+library(readr)
+library(tidyverse)
+library(ggplot2)
+# 4. 
+# Branch name:main
 
-# main
+olydata = read.csv("Olympics.csv")
+# 6.a.
+olydata1 = olydata %>%
+  mutate(total.medals = gold+silver+bronze)
