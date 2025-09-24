63da878fbc19a221416c8f306374e5f2b0ce5e57
ricardojoeson <1287883296@qq.com>
2025-09-24 14:24:09 -0500

    6.b.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -9,3 +9,8 @@ olydata = read.csv("Olympics.csv")
 # 6.a.
 olydata1 = olydata %>%
   mutate(total.medals = gold+silver+bronze)
+
+# 6.b.
+country_goldmedals = olydata %>%
+  group_by(country) %>%
+  summarise(gold_medals = sum(gold,na.rm = TRUE))
